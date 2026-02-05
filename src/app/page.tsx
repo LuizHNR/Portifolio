8db23a0508978181ba1a2f159d1c1c5f21547cd2
LuizHNR <luizhneri12@gmail.com>
2026-02-05 01:41:47 -0300

    new componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import Welcome from "@/components/welcome/welcome";
 import Particles  from "@/components/particles/Particles";
 import Image from "next/image";
 
+import LogoLoop from "@/components/logoLoop/LogoLoop";
+import {SiReact,SiNextdotjs,SiTypescript,SiTailwindcss,SiGithub,SiHtml5,SiCss3,SiJavascript,SiDotnet,SiPython,
+} from 'react-icons/si';
+
+
 import styles from "./page.module.css";
 
 async function delay(ms: number) {
@@ -11,13 +16,30 @@ async function delay(ms: number) {
 export default async function Home() {
   await delay(1800); 
 
+const techLogos = [
+  { node: <SiReact />, title: "React", href: "https://react.dev" },
+  { node: <SiNextdotjs />, title: "Next.js", href: "https://nextjs.org" },
+  { node: <SiTypescript />, title: "TypeScript", href: "https://www.typescriptlang.org" },
+  { node: <SiTailwindcss />, title: "Tailwind CSS", href: "https://tailwindcss.com" },
+
+  { node: <SiGithub />, title: "GitHub", href: "https://github.com" },
+  { node: <SiHtml5 />, title: "HTML5", href: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+  { node: <SiCss3 />, title: "CSS3", href: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+  { node: <SiJavascript />, title: "JavaScript", href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { node: <SiDotnet />, title: ".NET", href: "https://dotnet.microsoft.com" },
+  { node: <SiPython />, title: "Python", href: "https://www.python.org" },
+
+
+];
+
+
+
   return (
     <main>
       <Welcome />
 
       <section className={styles.localization}>
 
-        {/* FUNDO */}
         <div className={styles.particlesBg}>
           <Particles
             particleColors={["#ffffff"]}
@@ -54,6 +76,25 @@ export default async function Home() {
 
       </section>
 
+      <section className={styles.developer}>
+        <h1>Always studying so that I can become better every day!</h1>
+        <p>Taught to be a back-end and front-end programmer, to become a true full-stack developer.</p>
+      </section>
+
+
+      <div className={styles.logoLoopWrapper}>
+        <LogoLoop
+          logos={techLogos}
+          speed={100}
+          direction="left"
+          logoHeight={60}
+          gap={60}
+          hoverSpeed={0}
+          scaleOnHover
+          fadeOut
+          fadeOutColor="#ffffff"
+        />
+      </div>
 
 
     </main>
